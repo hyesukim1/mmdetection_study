1b062fb42df03ea404a96afd42275d6d7a7d7dc4
AI_09_김혜수 <94281700+hyesukim1@users.noreply.github.com>
2022-10-15 15:20:30 +0900

    Update practice.py

--- sample/practice.py
+++ sample/practice.py
@@ -7,17 +7,15 @@ import os
 # glob.glob은 경로명까지 전부 가져옴, os.listdir 파일명만 가져옴
 # glob은 풀패스가 아니라 검색을 지정한 폴더로부터 상대경로를 반환
 
-file_path = "C:/Users/kimsu/OneDrive/문서/GitHub/mmdetection_study/sample/json/"
+file_path = "C:/Users/kimsu/sample/json/"
 json_path ='*.json'
 img_path ='*.jpg'
-result_path = "C:/Users/kimsu/OneDrive/문서/GitHub/mmdetection_study/sample/result/"
-
-a = glob.glob(os.path.join(file_path, json_path)) # json 파일 전체 경로
-b = glob.glob(os.path.join(file_path, img_path)) # 이미지 파일 전체 경로
-
-# 
+result_path = "C:/Users/kimsu/sample/result"
+a = glob.glob(os.path.join(file_path, json_path))
+b = glob.glob(os.path.join(file_path, img_path))
+count = 1
 for json_file, img_file in zip(a, b):
-
+    count = count + 1
     with open(json_file, "r", encoding="UTF-8") as file:
         # print(json_file)
 
@@ -26,13 +24,12 @@ for json_file, img_file in zip(a, b):
         xmax = data["labelingInfo"][1]['box']['boundingBox']['Xmax']
         ymin = data["labelingInfo"][1]['box']['boundingBox']['Ymin']
         ymax = data["labelingInfo"][1]['box']['boundingBox']['Ymax']
-        print(xmin, xmax, ymin, ymax)
+        # print(xmin, xmax, ymin, ymax)
         
         img = cv2.imread(img_file)
-        cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (255,0,0), 5)
+        img_draw = cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (255,0,0), 5)
         # 이미지 보기
-        #cv2.imshow('image', img)
-        #cv2.waitKey(0) 
+        # cv2.imshow('rgb_image', img)
+        # cv2.waitKey(0) 
+        cv2.imwrite('C:/Users/kimsu/sample/result'+'\\'+str(count)+'.jpg', img_draw)
         
-        # 이미지 저장=> 반복문으로 해야하나?
-        # cv2.imwrite(os.path.join(result_path,'save_img_{}.jpg'.format(b)), img)
